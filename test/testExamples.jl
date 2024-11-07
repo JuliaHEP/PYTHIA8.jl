a24b251f3834a2a4875d71b7f763d05f6fc19784
Pere Mato <pere.mato@cern.ch>
2024-11-07 10:43:25 +0100

    Fix tests

--- test/testExamples.jl
+++ test/testExamples.jl
@@ -4,9 +4,9 @@
     Base.cd(dirname(dirname(pathof(PYTHIA8))))
 
     # All examples should be used for testing to ensure that the release is working correctly
-    @test Base.run(`julia --project=examples examples/main291.jl`, devnull, devnull).exitcode  == 0
-    @test Base.run(`julia --project=examples examples/main292.jl`, devnull, devnull).exitcode  == 0
-    @test Base.run(`julia --project=examples examples/main293.jl`, devnull, devnull).exitcode  == 0
-    @test Base.run(`julia --project=examples examples/main294.jl 11`, devnull, devnull).exitcode  == 0
-    @test Base.run(`julia --project=examples examples/main296.jl`, devnull, devnull).exitcode  == 0
+    @test Base.run(`julia --project examples/main291.jl`, devnull, devnull).exitcode  == 0
+    @test Base.run(`julia --project examples/main292.jl`, devnull, devnull).exitcode  == 0
+    @test Base.run(`julia --project examples/main293.jl`, devnull, devnull).exitcode  == 0
+    @test Base.run(`julia --project examples/main294.jl 11`, devnull, devnull).exitcode  == 0
+    @test Base.run(`julia --project examples/main296.jl`, devnull, devnull).exitcode  == 0
 end
